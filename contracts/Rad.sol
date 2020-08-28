c1f2ed7aa02985bc3dc48e3ce290ab34a8c34c1a
Alexis Sellier <self@cloudhead.io>
2020-08-28 18:27:37 +0200

    Remove max supply of Rad

--- contracts/Rad.sol
+++ contracts/Rad.sol
@@ -4,8 +4,6 @@ pragma solidity ^0.6.2;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Rad is ERC20 {
-    uint256 public constant MAX_SUPPLY = 100000000e18;
-
     /// Construct a new Rad token.
     ///
     /// @param account The initial account to grant all the tokens
@@ -14,11 +12,6 @@ contract Rad is ERC20 {
         public
         ERC20("Rad", "RAD")
     {
-        require(
-            initialSupply <= MAX_SUPPLY,
-            "The starting supply cannot be more than the max"
-        );
-
         _mint(account, initialSupply.mul(10**uint256(decimals())));
     }
 }
