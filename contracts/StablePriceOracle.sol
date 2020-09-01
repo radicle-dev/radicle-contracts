5d164b1510606979e2b91c1dce120de82d0366c3
Alexis Sellier <self@cloudhead.io>
2020-09-01 15:59:59 +0200

    Create price oracle using chainlink

---
+++ contracts/StablePriceOracle.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.6.12;
+
+// Mainnet ETHUSD: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+// Ropsten ETHUSD: 0x30B5068156688f818cEa0874B580206dFe081a03
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+import "./PriceOracle.sol";
+
+contract StablePriceOracle is PriceOracle {
+    /// Chainlink price oracle.
+    AggregatorV3Interface public priceFeed;
+
+    constructor(address _priceFeed) public {
+        priceFeed = AggregatorV3Interface(_priceFeed);
+    }
+
+    function latestPrice() public view override returns(int256) {
+        (, int price,,,) = priceFeed.latestRoundData();
+
+        return price;
+    }
+}
