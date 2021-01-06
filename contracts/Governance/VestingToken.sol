d0daab9ec16be22240608da2254ecedea52a2309
Alexis Sellier <self@cloudhead.io>
2021-01-06 18:15:07 +0100

    More cleanups on VestingToken

--- contracts/Governance/VestingToken.sol
+++ contracts/Governance/VestingToken.sol
@@ -9,50 +9,40 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Vesting {
     using SafeMath for uint256;
 
-    // FIELDS
-
-    // Constructor fields
-    ERC20 public MELON_CONTRACT; // MLN as ERC20 contract
+    ERC20 public token; // Radicle ERC20 contract
     address public owner; // deployer; can interrupt vesting
-    // Methods fields
+
     bool public interrupted; // whether vesting is still possible
     bool public isVestingStarted; // whether vesting period has begun
-    uint256 public totalVestingAmount; // quantity of vested Melon in total
+    uint256 public totalVestingAmount; // quantity of vested token in total
     uint256 public vestingStartTime; // timestamp when vesting is set
     uint256 public vestingPeriod; // total vesting period in seconds
     address public beneficiary; // address of the beneficiary
-    uint256 public withdrawn; // quantity of Melon withdrawn so far
+    uint256 public withdrawn; // quantity of token withdrawn so far
 
-    // MODIFIERS
-
-    modifier not_interrupted() {
+    modifier notInterrupted() {
         require(!interrupted, "The contract has been interrupted");
         _;
     }
 
-    modifier only_owner() {
+    modifier onlyOwner() {
         require(msg.sender == owner, "Only owner can do this");
         _;
     }
 
-    modifier only_beneficiary() {
+    modifier onlyBeneficiary() {
         require(msg.sender == beneficiary, "Only beneficiary can do this");
         _;
     }
 
-    modifier vesting_not_started() {
-        require(!isVestingStarted, "Vesting cannot be started");
-        _;
-    }
-
-    modifier vesting_started() {
+    modifier vestingStarted() {
         require(isVestingStarted, "Vesting must be started");
         _;
     }
 
-    /// @notice Calculates the quantity of Melon asset that's currently withdrawable
-    /// @return withdrawable Quantity of withdrawable Melon asset
-    function calculateWithdrawable() public view returns (uint256 withdrawable) {
+    /// @notice Returns the token amount that is currently withdrawable
+    /// @return withdrawable Quantity of withdrawable Radicle asset
+    function withdrawableBalance() public view returns (uint256 withdrawable) {
         uint256 timePassed = block.timestamp.sub(vestingStartTime);
 
         if (timePassed < vestingPeriod) {
@@ -63,55 +53,67 @@ contract Vesting {
         }
     }
 
-    /// @param ofMelonAsset Address of Melon asset
-    constructor(address ofMelonAsset, address ofOwner) {
-        MELON_CONTRACT = ERC20(ofMelonAsset);
-        owner = ofOwner;
+    /// @param _token Address of token being vested
+    constructor(address _token, address _owner) {
+        token = ERC20(_token);
+        owner = _owner;
     }
 
+    /// @notice Grant vesting tokens to the beneficiary.
+    ///
     /// @param ofBeneficiary Address of beneficiary
-    /// @param ofMelonQuantity Address of MLN asset
+    /// @param ofTokenAmount Amount of tokens
     /// @param ofVestingPeriod Vesting period in seconds from vestingStartTime
-    function setVesting(
+    function grantTokens(
         address ofBeneficiary,
-        uint256 ofMelonQuantity,
+        uint256 ofTokenAmount,
         uint256 ofVestingPeriod
-    ) external only_owner not_interrupted vesting_not_started {
-        require(ofMelonQuantity > 0, "Must vest some MLN");
+    ) external onlyOwner notInterrupted {
+        require(!isVestingStarted, "VestingToken::grantTokens: vesting already started");
+        require(ofTokenAmount > 0, "VestingToken::grantTokens: amount must be positive");
         require(
-            MELON_CONTRACT.transferFrom(msg.sender, address(this), ofMelonQuantity),
-            "MLN deposit failed"
+            token.transferFrom(msg.sender, address(this), ofTokenAmount),
+            "VestingToken::grantTokens: token deposit failed"
         );
+
         isVestingStarted = true;
         vestingStartTime = block.timestamp;
-        totalVestingAmount = ofMelonQuantity;
+        totalVestingAmount = ofTokenAmount;
         vestingPeriod = ofVestingPeriod;
         beneficiary = ofBeneficiary;
     }
 
-    /// @notice Withdraw
-    function withdraw() external only_beneficiary vesting_started not_interrupted {
-        uint256 withdrawable = calculateWithdrawable();
+    /// @notice Withdraw vested tokens
+    function withdrawVested() external onlyBeneficiary vestingStarted notInterrupted {
+        uint256 withdrawable = withdrawableBalance();
+
         withdrawn = withdrawn.add(withdrawable);
+
         require(
-            MELON_CONTRACT.transfer(beneficiary, withdrawable),
-            "Transfer to beneficiary failed"
+            token.transfer(beneficiary, withdrawable),
+            "VestingToken::withdrawVested: transfer to beneficiary failed"
         );
     }
 
-    /// @notice Withdraw vested tokens to beneficiary
+    /// @notice Force withdrawal of vested tokens to beneficiary
     /// @notice Send remainder back to owner
     /// @notice Prevent further vesting
-    function forceWithdrawalAndInterrupt() external only_owner vesting_started not_interrupted {
+    function terminateVesting() external onlyOwner vestingStarted notInterrupted {
         interrupted = true;
-        uint256 remainingVested = calculateWithdrawable();
+
+        uint256 remainingVested = withdrawableBalance();
         uint256 totalToBeVested = withdrawn.add(remainingVested);
         uint256 remainingUnvested = totalVestingAmount.sub(totalToBeVested);
+
         withdrawn = totalVestingAmount;
+
         require(
-            MELON_CONTRACT.transfer(beneficiary, remainingVested),
-            "Transfer to beneficiary failed"
+            token.transfer(beneficiary, remainingVested),
+            "VestingToken::terminateVesting: transfer to beneficiary failed"
         );
-        require(MELON_CONTRACT.transfer(owner, remainingUnvested), "Transfer to owner failed");
+        require(
+            token.transfer(owner, remainingUnvested),
+            "VestingToken::terminateVesting: transfer to owner failed"
+        );
     }
 }
