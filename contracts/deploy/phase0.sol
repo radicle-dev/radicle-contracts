33c83601d0dcb93dd080e558acd0fc81bf151803
Alexis Sellier <self@cloudhead.io>
2021-02-15 21:05:09 +0100

    Prettier

--- contracts/deploy/phase0.sol
+++ contracts/deploy/phase0.sol
@@ -55,7 +55,8 @@ contract Phase0 {
         _token.transfer(address(_timelock), TREASURY_ALLOCATION);
         require(_token.balanceOf(address(this)) == 0, "All tokens are allocated");
         require(
-            MONADIC_ALLOCATION + FOUNDATION_ALLOCATION + TREASURY_ALLOCATION == _token.totalSupply(),
+            MONADIC_ALLOCATION + FOUNDATION_ALLOCATION + TREASURY_ALLOCATION ==
+                _token.totalSupply(),
             "All tokens are allocated"
         );
 
